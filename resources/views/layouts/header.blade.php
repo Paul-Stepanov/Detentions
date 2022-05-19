35826383972653d80195c06c835d3cb91fee58d1
Pavel Stepanov <p.stepanov13@mail.ru>
2022-05-19 17:00:58 +0300

    скрыты ссылки header для пользователей не являющихся администраторами

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,29 +3,33 @@
       <img src="{{ asset('/img/logo.png') }}" alt="logo">
 
    </div>
-      @auth
-         <a class="nav__menu-item-link nav__menu-item-link--username" href="{{ route('login') }}">{{ Auth::user() -> name }}</a>
-      @endauth
-   <nav class="header__nav nav">
-      <ul class="nav__menu">
-         <li class="nav__menu-item"><a href="{{ route('detention.index') }}" class="nav__menu-item-link">Задержания</a>
-         </li>
-         <li class="nav__menu-item"><a href="{{ route('division.index') }}" class="nav__menu-item-link">Список
-               подразделений</a></li>
-         <li class="nav__menu-item"><a href="{{ route('type.index') }}" class="nav__menu-item-link">Виды задержаний</a>
-         </li>
-         <li class="nav__menu-item"><a href="{{ route('note.index') }}" class="nav__menu-item-link">Основания для
-               прекращения
-               регистрации ТС</a>
-         </li>
-         <li class="nav__menu-item">
-            <a class="nav__menu-item-link" href="{{ route('register') }}">Регистрация пользователя</a>
-         </li>
-         <a class="nav__menu-item nav__menu-item--search" href='{{ route('search.showForm') }}' id="searchButton">
-            <span>Поиск</span>
-            <img src="{{ asset('img/icons/search-icon.png') }}" alt="search">
-         </a>
-         @auth
+   @auth
+      <a class="nav__menu-item-link nav__menu-item-link--username"
+         href="{{ route('login') }}">{{ Auth::user() -> name }}</a>
+      <nav class="header__nav nav">
+         <ul class="nav__menu">
+            <li class="nav__menu-item"><a href="{{ route('detention.index') }}"
+                                          class="nav__menu-item-link">Задержания</a>
+            </li>
+            @if(auth()->user()->role)
+
+            <li class="nav__menu-item"><a href="{{ route('division.index') }}" class="nav__menu-item-link">Список
+                  подразделений</a></li>
+            <li class="nav__menu-item"><a href="{{ route('type.index') }}" class="nav__menu-item-link">Виды
+                  задержаний</a>
+            </li>
+            <li class="nav__menu-item"><a href="{{ route('note.index') }}" class="nav__menu-item-link">Основания для
+                  прекращения
+                  регистрации ТС</a>
+            </li>
+               <li class="nav__menu-item">
+                  <a class="nav__menu-item-link" href="{{ route('register') }}">Регистрация пользователя</a>
+               </li>
+            @endif
+            <a class="nav__menu-item nav__menu-item--search" href='{{ route('search.showForm') }}' id="searchButton">
+               <span>Поиск</span>
+               <img src="{{ asset('img/icons/search-icon.png') }}" alt="search">
+            </a>
             <li class="nav__menu-item button__logout">
                <a class=" nav__menu-item-link" href="{{ route('logout') }}"
                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
@@ -34,7 +38,7 @@
                   @csrf
                </form>
             </li>
-         @endauth
-      </ul>
-   </nav>
+            @endauth
+         </ul>
+      </nav>
 </header>
