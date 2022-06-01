1d20ec5fbd4e6816ce7b4d7549497d63a7040b14
Pavel Stepanov <p.stepanov13@mail.ru>
2022-06-01 11:02:31 +0300

    обновление версии в футере

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-   <p class="footer__text"> © Управление ГИБДД УМВД России по Тульской области 2022  (version 1.0)</p>
+   <p class="footer__text"> © Управление ГИБДД УМВД России по Тульской области 2022  (version 1.2)</p>
 </footer>
